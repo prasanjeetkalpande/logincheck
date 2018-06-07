a254e98abd922b74dec4585667cda7ff738513b0
prasanjeetkalpande <38899631+prasanjeetkalpande@users.noreply.github.com>
2018-06-07 17:45:23 +0530

    Create index.js
    fhf

---
+++ index.js
@@ -0,0 +1,19 @@
+function myFunction() {
+ var name=document.getElementById('um').value;
+  var password=document.getElementById('pass').value;
+ 
+ 
+  if (name=="amar" && password=="pass") {
+         document.getElementById("dis").innerHTML="succesfully";
+    //window.location.replace("https://www.w3schools.com")
+  
+    window.location = "https://prasanjeetkalpande.github.io/streetlight/";
+//window.location.href = "https://www.w3schools.com";
+        // window.open("https://www.w3schools.com");
+
+          // Window.close();
+        }
+  else {
+         document.getElementById("dis").innerHTML="Invalide data";
+       }
+}
